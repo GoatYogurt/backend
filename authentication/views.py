1edd555e886632cf2a1d95a0236a47fbbd17e060
2uan2 <2uan20004@gmail.com>
2025-05-11 00:34:09 +0700

    added view for getting token for guest

--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,5 @@
+import uuid
+
 from rest_framework.authtoken.models import Token
 from rest_framework.authtoken.serializers import AuthTokenSerializer
 
@@ -11,8 +13,14 @@ from rest_framework import status
 from .serializers import RegisterSerializer, CallTokenSerializer
 
 import stream_chat
+import getstream
+from getstream.models import UserRequest
 from django.conf import settings
 
+client = getstream.Stream(
+    api_key=settings.API_KEY,
+    api_secret=settings.API_SECRET,
+)
 class AuthTokenView(generics.ListCreateAPIView):
     permission_classes = [IsAuthenticated]
     serializer_class = AuthTokenSerializer
@@ -26,7 +34,7 @@ class AuthTokenView(generics.ListCreateAPIView):
         if not request.user.is_superuser:
             return Response('You do not have permission to view all tokens', status=status.HTTP_403_FORBIDDEN)
         
-        tokens = self.get_queryset()
+        auth_tokens = self.get_queryset()
         data = [
             {
                 'user_id': token.user.id,
@@ -34,7 +42,7 @@ class AuthTokenView(generics.ListCreateAPIView):
                 'email': token.user.email,
                 'token': token.key
             }
-            for token in tokens
+            for token in auth_tokens
         ]
         return Response(data)
     
@@ -42,9 +50,9 @@ class AuthTokenView(generics.ListCreateAPIView):
         serializer = self.serializer_class(data=request.data, context={'request': request})
         serializer.is_valid(raise_exception=True)
         user = serializer.validated_data['user']
-        token, created = Token.objects.get_or_create(user=user)
+        auth_token, created = Token.objects.get_or_create(user=user)
         return Response({
-            'token': token.key,
+            'token': auth_token.key,
             'user_id': user.pk,
             'email': user.email
         })
@@ -53,16 +61,6 @@ class AuthTokenView(generics.ListCreateAPIView):
 class RegisterView(generics.CreateAPIView):
     permission_classes = [AllowAny]
     serializer_class = RegisterSerializer
-
-    def post(self, request):
-        serializer = RegisterSerializer(data=request.data)
-        if serializer.is_valid():
-            user = serializer.save()
-            token, created = Token.objects.get_or_create(user=user)
-            return Response({"message": "User registered successfully.",
-                              "token": token.key,
-                            }, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
     def get(self, request):
         return Response({
@@ -70,6 +68,23 @@ class RegisterView(generics.CreateAPIView):
             "email": "string (optional)",
             "password": "string"
         })
+
+    def post(self, request):
+        serializer = RegisterSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            client.upsert_users(
+                UserRequest(
+                    id=user.username, name=user.username, role="user"
+                ),
+            )
+            stream_token = client.create_token(user_id=user.username)
+            auth_token, created = Token.objects.get_or_create(user=user)
+            return Response({"username": user.username,
+                              "auth_token": auth_token.key,
+                              "stream_token": stream_token
+                            }, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class LoginView(generics.CreateAPIView):
@@ -80,12 +95,15 @@ class LoginView(generics.CreateAPIView):
         serializer = self.serializer_class(data=request.data, context={'request': request})
         serializer.is_valid(raise_exception=True)
         user = serializer.validated_data['user']
-        token, created = Token.objects.get_or_create(user=user)
+        auth_token, created = Token.objects.get_or_create(user=user)
+        stream_token = client.create_token(user_id=user.username)
+
         return Response({
-            "token": token.key,
-            "user_id": user.id,
             "username": user.username,
-            "email": user.email,
+            "auth_token": auth_token.key,
+            "stream_token": stream_token,
+            # "user_id": user.id,
+            # "email": user.email,
         })
 
     def get(self, request):
@@ -100,10 +118,43 @@ class CallTokenView(generics.CreateAPIView):
     serializer_class = CallTokenSerializer
 
     def get(self, request):
-        server_client = stream_chat.StreamChat(
-            api_key=settings.API_KEY, 
-            api_secret=settings.API_SECRET
+        client = getstream.Stream(
+            api_key=settings.API_KEY,
+            api_secret=settings.API_SECRET,
         )
+        
+        stream_token = client.create_token(user_id=request.user.username)
+        return Response({ "stream_token": stream_token })
 
-        token = server_client.create_token(request.user.username)
-        return Response(token)
+
+class GuestUserView(APIView):
+    permission_classes = [AllowAny]
+
+    def get(self, request):
+        guest_id = f"Guest-{uuid.uuid4()}"
+        stream_token = client.create_token(user_id=guest_id, expiration=3600)
+
+        return Response({
+            "guest_id": guest_id,
+            "stream_token": stream_token
+        })
+
+
+class CallQueryView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        calls = client.video.query_calls(
+            filter_conditions={'ongoing': {'$eq': True}}
+        )
+        # print(calls.data.calls)
+        # response = client.video.query_calls(
+        #     sort= [SortParamRequest(field: 'starts_at', direction: -1)],
+        #     limit=2,
+        # )
+        call_ids = []
+        for call in calls.data.calls:
+            call_ids.append(call.call.id)
+            # print('\n')
+        return Response({ "calls": call_ids })
+
