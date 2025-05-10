6e0e22c561e3b068d5cdd417db585cff7038f6a2
2uan2 <2uan20004@gmail.com>
2025-05-11 00:34:39 +0700

    small change to make token authentication work

--- backend/settings.py
+++ backend/settings.py
@@ -53,9 +53,10 @@ INSTALLED_APPS = [
 REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
     # or allow read-only access for unauthenticated users.
-    'DEFAULT_PERMISSION_CLASSES': [
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
         'rest_framework.authentication.BasicAuthentication',
-        'rest_framework.authentication.SessionAuthentication'
+        'rest_framework.authentication.SessionAuthentication',
     ]
 }
 
